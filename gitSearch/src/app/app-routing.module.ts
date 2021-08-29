851f1838e5307afed941c77df1a83c95be8085f4
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-08-29 20:08:18 +0300

    modified the navbar folder

--- gitSearch/src/app/app-routing.module.ts
+++ gitSearch/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './profile/profile.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { ReposComponent } from './repos/repos.component';
+import { SearchComponent } from './search/search.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'profile', component: ProfileComponent},
+{ path: 'repo', component: ReposComponent},
+{ path: 'search', component: SearchComponent},
+{ path:'**', component:NotFoundComponent},
+{ path: '', redirectTo:"/gitsearch", pathMatch:"full"},
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
