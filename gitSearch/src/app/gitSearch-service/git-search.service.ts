0ad45ee8c232ee015810e97a0909d408397aa34b
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-08-29 10:53:12 +0300

    modified the service .ts

--- gitSearch/src/app/gitSearch-service/git-search.service.ts
+++ gitSearch/src/app/gitSearch-service/git-search.service.ts
@@ -8,17 +8,6 @@ import { Repository } from 'src/represitory';
   providedIn: 'root'
 })
 export class GitSearchService {
-  getRepos() {
-    throw new Error('Method not implemented.');
-  }
-  getMyRepos: any;
-  getMyUser() {
-    throw new Error('Method not implemented.');
-  }
-  getUser() {
-    throw new Error('Method not implemented.');
-  }
-
 
     public userName: string;
     private endpoint: string = "/users/";
@@ -26,121 +15,116 @@ export class GitSearchService {
     public user: User;
     public repository: Repository;
     public reposArray: any = [];
-  updateSearchTerm: any;
-  searchRepos: any;
-  updateProfile: any;
 
     constructor(private http:HttpClient) {
-        this.userName = "Maureen-1998DEV";
+        this.userName = "Maureem-1998DEV";
         this.user= new User("","","","","");
         this.repository=  new Repository("","","");
     }
 
-    // getMyUser () { 
-    //     interface ApiResponse(){}
-    //     let promise = new Promise<void> ((resolve, reject) => {
-    //         this.http.get(environment.urlApikey + this.endpoint + "Maureen-1998DEV").toPromise().then(
-    //             res => {
-    //                 this.user.userName = res.userName;
-    //                 this.user.login = res.login;
-    //                 this.user.profile = res.profile;
-    //                 this.user.links = res.html_url;
-    //                 this.user.avatar = res.avatar_;
-    //                 resolve();
-    //             }, error => {
-    //                 reject(error);
-    //             }
-    //         );
-    //     });
-    //     return promise;
-    // }
-     }
+    getMyUser () {
+        let promise = new Promise<void> ((resolve, reject) => {
+            this.http.get(environment.urlApikey + this.endpoint + "Maureen-1998DEV" ).toPromise().then(
+                res => {
+                    this.user.userName = res.json().name;
+                    this.user.login = res.json().login;
+                    this.user.profile = res.json().profile;
+                    this.user.links = res.json().html_url;
+                    this.user.avatar = res.json().avatar_url;
+                    resolve();
+                }, error => {
+                    reject(error);
+                }
+            );
+        });
+        return promise;
+    }
 
+    getUser() {
+        let promise = new Promise<void> ((resolve, reject) => {
+            this.http.get(environment.urlApikey + this.endpoint + this.userName + "?client_id=").toPromise().then(
+                res => {
+                    this.user.userName = res.json().userName;
+                    this.user.login = res.json().login;
+                    this.user.profile = res.json().profile;
+                    this.user.links = res.json().html_url;
+                    this.user.avatar = res.json().avatar_url;
+                    resolve();
+                }, error => {
+                    reject(error);
+                }
+            );
+        });
+        return promise;
+    }
 
-//     getUser() {
-//         let promise = new Promise<void> ((resolve, reject) => {
-//             this.http.get(environment.urlApikey + this.endpoint + this.userName ).toPromise().then(
-//                 res => {
-//                     this.user.user = res.json().name;
-//                     this.user.login = res.json().login;
-//                     this.user.profile = res.json().profile;
-//                     this.user.links = res.json().html_url;
-//                     this.user.avatar = res.json().avatar_url;
-//                     resolve();
-//                 }, error => {
-//                     reject(error);
-//                 }
-//             );
-//         });
-//         return promise;
-//     }
+    getMyRepos() {
+        let promise = new Promise<void>((resolve, reject) => {
+            this.http.get(environment.urlApikey + this.endpoint + "Maureen-1998DEV" + "/repos?client_id=" ).toPromise().then(
+                res => {
+                    for (let repo of res.json()){
+                        this.repository.Reponame = repo.name;
+                        this.repository.description = repo.description;
+                        this.repository.repolinks = repo.html_url;
+                        this.reposArray.push(this.repository);
+                        this.repository=  new Repository("","","");
+                        resolve();
+                    }
+                }, error => {
+                    reject(error);
+                }
+            );
+        });
+        return promise;
+    }
 
-//     getMyRepos() {
-//         let promise = new Promise<void>((resolve, reject) => {
-//             this.http.get(environment.urlApikey + this.endpoint + "Maureen-1998DEV" ).toPromise().then(
-//                 res => {
-//                     for (let repo of res.json()){
-//                         this.repository.name = repo.name;
-//                         this.repository.description = repo.description;
-//                         this.repository.repoLink = repo.html_url;
-//                         this.reposArray.push(this.repository);
-//                         this.repository=  new Repository("","","");
-//                         resolve();
-//                     }
-//                 }, error => {
-//                     reject(error);
-//                 }
-//             );
-//         });
-//         return promise;
-//     }
+    getRepos() {
+        this.reposArray = [];
+        let promise = new Promise<void>((resolve, reject) => {
+            this.http.get(environment.urlApikey + this.endpoint + this.userName + "/repos?client_id=" ).toPromise().then(
+                res => {
+                    for (let repo of res.json()){
+                        this.repository.Reponame = repo.Reponame;
+                        this.repository.description = repo.description;
+                        this.repository.repolinks = repo.html_url;
+                        this.reposArray.push(this.repository);
+                        this.repository=  new Repository("","","");
+                        resolve();
+                    }
+                }, error => {
+                    reject(error);
+                }
+            );
+        });
+        return promise;
+    }
 
-//     getRepos() {
-//         this.reposArray = [];
-//         let promise = new Promise<void>((resolve, reject) => {
-//             this.http.get(environment.urlApikey + this.endpoint + this.userName).toPromise().then(
-//                 res => {
-//                     for (let repo of res.json()){
-//                         this.repository.name = repo.name;
-//                         this.repository.description = repo.description;
-//                         this.repository.repoLink = repo.html_url;
-//                         this.reposArray.push(this.repository);
-//                         this.repository=  new Repository("","","");
-//                         resolve();
-//                     }
-//                 }, error => {
-//                     reject(error);
-//                 }
-//             );
-//         });
-//         return promise;
-//     }
+    searchRepos() {
+        this.reposArray = [];
+        let promise =new Promise<void>((resolve, reject) => {
+            this.http.get(environment.urlApikey + "/search/repositories?q=" + this.searchTerm + "in:name").toPromise().then(
+                res => {
+                    for (let repo of res.json().items){
+                        this.repository.Reponame = repo.Reponame;
+                        this.repository.description = repo.description;
+                        this.repository.repolinks = repo.html_url;
+                        this.reposArray.push(this.repository);
+                        this.repository=  new Repository("","","");
+                        resolve();
+                    }
+                }, error => {
+                    reject(error);
+                }
+            )
+        })
+    }
 
-//     searchRepos() {
-//         this.reposArray = [];
-//         let promise =new Promise<void>((resolve, reject) => {
-//             this.http.get(environment.urlApikey + "/search/repositories?q=" + this.searchTerm + "in:name").toPromise().then(
-//                 res => {
-//                     for (let repo of res.json().items){
-//                         this.repository.name = repo.name;
-//                         this.repository.description = repo.description;
-//                         this.repository.repo= repo.html_url;
-//                         this.reposArray.push(this.repository);
-//                         this.repository=  new Repository("","","");
-//                         resolve();
-//                     }
-//                 }, error => {
-//                     reject(error);
-//                 }
-//             )
-//         })
-//     }
+    updateSearchTerm(searchTerm:string) {
+        this.searchTerm = searchTerm;
+    }
 
-//     updateSearchTerm(searchTerm:string) {
-//         this.searchTerm = searchTerm;
-//     }
+    updateProfile(username:string) {
+        this.userName = username;
+    }
 
-//     updateProfile(username:string) {
-//         this.userName = username;
-//     }
-// }
+}
