596348ab282fb7bb452ed3a9c20ccc874eea8dd2
Maureen Ougo <maureen.ougo@student.moringaschool.com>
2021-08-30 02:04:44 +0300

    modified the README.md file

--- gitSearch/src/app/app.module.ts
+++ gitSearch/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProfileComponent } from './profile/profile.component';
+import { SearchComponent } from './search/search.component';
+import { ReposComponent } from './repos/repos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ProfileComponent,
+    SearchComponent,
+    ReposComponent
   ],
   imports: [
     BrowserModule
